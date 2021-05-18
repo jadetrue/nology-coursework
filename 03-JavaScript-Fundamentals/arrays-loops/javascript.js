aed351217c6ab1fe00c0e256328d2c3ee3ced554
Jade True <jadetruexo@gmail.com>
2021-05-18 10:16:47 +0100

    all changes from laptop

--- 03-JavaScript-Fundamentals/arrays-loops/javascript.js
+++ 03-JavaScript-Fundamentals/arrays-loops/javascript.js
@@ -4,10 +4,22 @@ const pets = ["Frankie", "Benson"];
 
 //console.log(pets[1]);
 
-for (index = 0; index < coaches.length; index++) {
-    console.log(coaches[index]);
-}
+// for (index = 0; index < coaches.length; index++) {
+//     console.log(coaches[index]);
+// }
 
-for (index = 0; index < pets.length; index++) {
-    console.log(pets[index])
-}
+// for (index = 0; index < pets.length; index++) {
+//     console.log(pets[index])
+// }
+
+// const newCoaches = coaches.map((coach) => {
+//      return "Coach - " + coach;
+// })
+
+// console.log(newCoaches)
+
+const newPet = pets.map((pet) => {
+    return "My pets names are " + pet;
+})
+
+console.log(newPet);
---
+++ 03-JavaScript-Fundamentals/arrays-loops/script.js
@@ -0,0 +1,17 @@
+// const names = ["Andy", "Bex", "Ollie"];
+
+// const coaches = names.filter((name) => {
+//     if (name.includes("e")) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// })
+
+// console.log(coaches);
+
+const scores = [9, 10, 18];
+
+const highScore = scores.filter((score) => score > 10);
+
+console.log(highScore)
