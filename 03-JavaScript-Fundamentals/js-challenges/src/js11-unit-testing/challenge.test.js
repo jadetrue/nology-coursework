4a9c0843ecc298f59569e536ce847a6f187f068f
Jade True <jadetruexo@gmail.com>
2021-08-30 21:00:24 +0100

    Js11 Challenges

--- 03-JavaScript-Fundamentals/js-challenges/src/js11-unit-testing/challenge.test.js
+++ 03-JavaScript-Fundamentals/js-challenges/src/js11-unit-testing/challenge.test.js
@@ -1,1 +1,8 @@
-import * as challenge from "./js13";
+import * as challenge from './js13';
+
+const {greet} = challenge;
+
+it('Should return a string "Hello, nice to meet you"', () => {
+    const output = greet();
+    expect(output).toBe('Hello, nice to meet you');
+});
