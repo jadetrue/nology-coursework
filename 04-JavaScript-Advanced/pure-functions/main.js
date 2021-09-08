73e4b95b1c251eab578f29318cf014910a0511ec
Jade True <jadetruexo@gmail.com>
2021-09-08 17:05:11 +0100

    Pure functions

---
+++ 04-JavaScript-Advanced/pure-functions/main.js
@@ -0,0 +1,90 @@
+// document.querySelector('.btn').addEventListener('click', () => {
+//     const input = document.querySelector('.input').value;
+
+//     if (input === 'Hello') {
+//         document.querySelector('.result').innerHTML = 'Bonjur';
+//     } else if (input === 'Goodbye') {
+//         document.querySelector('.result').innerHTML = 'Au Revoir';
+//     } else {
+//         document.querySelector('.result').innerHTML = 'Word not recognised';
+//     }
+// });
+
+// Purified code :
+
+const translator = (input) => {
+    if (input === 'Hello') {
+        return 'Bonjur';
+    }
+    if (input === 'Goodbye') {
+        return 'Au Revior';
+    } else return 'Word not recognised';
+};
+
+const btn = document.querySelector('.btn').addEventListener('click', () => {
+    const value = document.querySelector('.input').value;
+    const result = (document.querySelector('.result').innerHTML =
+        translator(value));
+});
+
+// Challenge:
+const aDate = new Date('02/16/2021');
+
+const doesLotsOfThings = (firstName, lastName, birthday, arrayName) => {
+    const fullName = firstName.toUpperCase() + ' ' + lastName.toUpperCase();
+    console.log(fullName);
+
+    const bday = new Date(birthday);
+    const diff = aDate.getTime() - bday.getTime();
+
+    let ms = 1000 * 3600 * 24;
+
+    const result = diff / ms;
+    console.log(result);
+
+    let myArray = [];
+    arrayName.forEach((item) => {
+        if (item.includes('Tech')) {
+            return myArray.push(item);
+        } else {
+            console.log('nope not tech');
+        }
+    });
+};
+
+const test = doesLotsOfThings('matt', 'Bickell', '09/26/1993', [
+    'Tech is booming',
+    'Covid 19 yada yada yada',
+    'Economy due for a resurgance!',
+    'You can retrain in tech too!',
+]);
+
+// MY CODE
+
+const person = {
+    firstName: 'Matt',
+    surname: 'Bickell',
+    birthday: '09/26/1993',
+    quotes: [
+        'Tech is booming',
+        'Covid 19 yada yada yada',
+        'Economy due for a resurgance!',
+        'You can retrain in tech too!',
+    ],
+};
+
+const fullName = () => {
+    return person.firstName.toUpperCase() + ' ' + person.surname.toUpperCase();
+};
+
+const birthday = () => {
+    const randomDate = (person.birthday = new Date());
+    let ms = 1000 * 3600 * 24;
+
+    return randomDate / ms;
+};
+
+console.log('---------------------------------');
+
+console.log(fullName());
+console.log(birthday());
