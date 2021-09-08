c6636b5c52341c2b7377cb512e89fd4dc4427e75
Jade True <jadetruexo@gmail.com>
2021-09-08 20:54:54 +0100

    Promises

---
+++ 04-JavaScript-Advanced/promises/app.js
@@ -0,0 +1,34 @@
+// console.log('hello');
+
+// let promise = new Promise((resolve, reject) => {
+//     let a = 1 + 2;
+//     if (a === 2) {
+//         resolve('YAY');
+//     } else {
+//         reject('boo');
+//     }
+// });
+
+// promise
+//     .then((message) => {
+//         console.log(`${message} I am in the then`);
+//     })
+//     .catch((message) => {
+//         console.log(`${message} I am caught`);
+//     });
+
+// https://randomuser.me/api
+
+// const heading = document.querySelector('h1');
+
+// const getRandomUser = () => {
+//     fetch('https://randomuser.me/api')
+//         .then((res) => res.json())
+//         .then((json) => (heading.innerHTML = json.results[0].name.first))
+//         .catch((err) => console.log(err));
+// };
+
+// getRandomUser();
+
+// https://api.punkapi.com/v2/beers/random
+// name, tagline, description, abv, first food it pairs well with
