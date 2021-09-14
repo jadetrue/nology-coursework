b9afc7e49e70f09522ec266a854834060f03d949
Jade True <jadetruexo@gmail.com>
2021-09-14 21:25:38 +0100

    Created a card from the beers api

--- 04-JavaScript-Advanced/drink-app/app.js
+++ 04-JavaScript-Advanced/drink-app/app.js
@@ -2,3 +2,27 @@
 
 const heading = document.querySelector('h1');
 const tagline = document.querySelector('h2');
+const description = document.querySelector('h3');
+const abv = document.querySelector('span');
+const foodPairing = document.querySelector('h4');
+
+const getRandomDrink = () => {
+    fetch('https://api.punkapi.com/v2/beers/random')
+        .then((response) => response.json())
+        .then((data) => {
+            data.map((items) => {
+                return console.log(
+                    (data = [
+                        (heading.innerHTML = items.name),
+                        (tagline.innerHTML = items.tagline),
+                        (description.innerHTML = items.description),
+                        (abv.innerHTML = `${items.abv}%`),
+                        (foodPairing.innerHTML = `Pairs well with: ${items.food_pairing[0]}`),
+                    ])
+                );
+            });
+        })
+        .catch((err) => console.log(err));
+};
+
+getRandomDrink();
