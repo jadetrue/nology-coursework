425c1d03a4ab2669e8c2e496b1bfeb74cfa50d80
Jade True <jadetruexo@gmail.com>
2021-09-20 15:04:58 +0100

    Changed to use classes

--- 04-JavaScript-Advanced/morse-code-translator/index.js
+++ 04-JavaScript-Advanced/morse-code-translator/index.js
@@ -4,15 +4,42 @@
 
 // Grab the variables I want to interact with
 
-const button = document.querySelector("button");
+const translateBtn = document.querySelector(".translate");
 const input = document.querySelector("input");
 const output = document.querySelector("output");
+const sectionTwo = document.querySelector(".s2");
 
-// Add an event listener to the button for on click
+const switchBtn = document.querySelector(".switch");
+const switchTitle = document.querySelector("h4");
 
-button.addEventListener("click", (e) => {
-    e.preventDefault();
-    button.classList.toggle("btn-clicked");
-    // Display the value in the output
-    output.innerHTML = input.value;
+// Created my class for the Translator
+
+class Translator {
+    constructor() {}
+
+    switchTitle() {
+        if (switchTitle.innerHTML === "English to Morse Code") {
+            switchTitle.innerHTML = "Morse Code to English";
+        } else {
+            switchTitle.innerHTML = "English to Morse Code";
+        }
+    }
+
+    handleTranslateBtn() {
+        translateBtn.classList.toggle("btn-clicked");
+        sectionTwo.classList.toggle("s2-toggled");
+        // Display the value in the output
+        output.innerHTML = input.value;
+    }
+}
+
+// Created a new translator
+const translator = new Translator();
+
+switchBtn.addEventListener("click", () => {
+    translator.switchTitle();
+});
+
+translateBtn.addEventListener("click", () => {
+    translator.handleTranslateBtn();
 });
