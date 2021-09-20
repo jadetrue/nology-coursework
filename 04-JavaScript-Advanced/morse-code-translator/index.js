df7fb8088a3a7c8a36965fa1faf183c93ebbf530
Jade True <jadetruexo@gmail.com>
2021-09-20 13:49:09 +0100

    Added an Event Listener to my button

--- 04-JavaScript-Advanced/morse-code-translator/index.js
+++ 04-JavaScript-Advanced/morse-code-translator/index.js
@@ -1,1 +1,18 @@
 "use strict";
+
+// Breakdown
+
+// Grab the variables I want to interact with
+
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+const output = document.querySelector("output");
+
+// Add an event listener to the button for on click
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    button.classList.toggle("btn-clicked");
+    // Display the value in the output
+    output.innerHTML = input.value;
+});
