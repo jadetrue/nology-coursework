2cf4b8a1b719c6047b10e14d620fb55f481c60f1
Jade True <jadetruexo@gmail.com>
2021-12-13 15:44:27 +0000

    firebase info

---
+++ 07-Client-Project-Tools/firebase-test/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAW6QRrwKSCxoFrXf5y_gC00jb-exLo8mA",
+    authDomain: "nology-io.firebaseapp.com",
+    projectId: "nology-io",
+    storageBucket: "nology-io.appspot.com",
+    messagingSenderId: "158961884320",
+    appId: "1:158961884320:web:3709b7d88f4355b0f5fdc5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
